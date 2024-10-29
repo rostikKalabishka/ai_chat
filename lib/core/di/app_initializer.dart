6022a9e8ab2f63355584bb12bc60496e9bd3ee72
RostikKalabishka <rostik31052002@gmail.com>
2024-10-29 07:08:29 +0200

    test

--- lib/core/di/app_initializer.dart
+++ lib/core/di/app_initializer.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
+//test
 class AppInitializer extends StatelessWidget {
   const AppInitializer({super.key, required this.child});
   final Widget child;
